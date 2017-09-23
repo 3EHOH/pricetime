cb321c71a447b5eea01ad865b88904410f455219
3EHOH <zenonevans@gmail.com>
2017-09-22 21:31:24 -0500

    add exception handler to day checker

--- src/main/scala/com/zenon/pricetime/HelloWorld.scala
+++ src/main/scala/com/zenon/pricetime/HelloWorld.scala
@@ -9,10 +9,21 @@ import org.joda.time._
 
 object HelloWorld {
   val service = HttpService {
-    case GET -> Root / "hello" / name =>
-      val dateTime: DateTime = DateTime.parse(name)
-      val dayOfWeek: String = dateTime.dayOfWeek().getAsShortText()
+    case GET -> Root / "isodatetime" / name =>
+      val dayOfWeek: String = maybeExtractDayOfWeek(name)
       Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}, ${dayOfWeek}")))
-      //Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+  }
+
+  def extractDayOfWeek(dateInput: String) : String = {
+    val dateTime: DateTime = DateTime.parse(dateInput)
+    dateTime.dayOfWeek.getAsShortText.toLowerCase
+  }
+
+  def maybeExtractDayOfWeek(dateInput: String) : String = {
+    try {
+      extractDayOfWeek(dateInput)
+    } catch {
+      case e: Exception => s"Exception caught: ${e}";
+    }
   }
 }
